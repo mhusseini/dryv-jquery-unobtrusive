104f801e44e81fb8449ad1151552f035243689b7
Munir Husseini <m.husseini@gmx.de>
2018-03-11 22:15:14 +0100

    Building dist outputs for UMS and for browsers.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,33 @@
 const gulp = require("gulp");
-const ts = require('gulp-typescript');
-const uglify = require('gulp-uglify');
-const pump = require('pump');
+const ts = require("gulp-typescript");
+const uglify = require("gulp-uglify");
+const pump = require("pump");
 const rename = require("gulp-rename");
+const replace = require("gulp-replace");
 
-gulp.task("build", done => {
-    const tsProject = ts.createProject("tsconfig.json");
-    pump([
+gulp.task("default", ["build-module", "build-browser"]);
+
+gulp.task("build-module", done => {
+    build("umd", "dryv-jquery-unobtrusive", done, 'import * as $ from "jquery";');
+});
+
+gulp.task("build-browser", done => {
+    build("none", "dryv-jquery-unobtrusive.browser", done, '///<reference types="jquery" />');
+});
+
+function build(moduleType, basename, done, importCommand) {
+    const tsProject = ts.createProject("tsconfig.json", { module: moduleType });
+    const streams = [
         gulp.src("src/**/*.ts"),
+        replace('///<reference types="jquery" />', importCommand),
+        replace(/\/\/\/.*/g, ""),
         tsProject(),
-        rename({ basename: "dryv-jquery-unobtrusive" }),
+        rename({ basename: basename }),
         gulp.dest("dist"),
         uglify(),
         rename({ suffix: ".min" }),
         gulp.dest("dist")
-    ], done);
-});
+    ];
+
+    return pump(streams, done);
+}
